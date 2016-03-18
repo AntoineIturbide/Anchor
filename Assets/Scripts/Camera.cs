f99d2297d174abf4d626d04c3bd4700912a1745b
AntoineIturbide <antoine.iturbide@gmail.com>
2016-03-18 17:45:28 +0100

    Camera script beggined

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Character {
+
+    public class Camera : MonoBehaviour {
+
+        Vector2 lastMousePos = Vector3.zero;
+
+        void LateUpdate() {
+
+            Input.GetAxis("MouseX");
+
+        }
+
+    }
+
+}
