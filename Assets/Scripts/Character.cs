873eb7d9ee8a1be872f562cf681ec0be8ae78b4c
AntoineIturbide <antoine.iturbide@gmail.com>
2016-03-18 18:32:51 +0100

    Update Controller Script

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Controller {
+    [RequireComponent(typeof(CharacterController))]
+    public class Character : MonoBehaviour {
+        /** CONFIGURATION **/
+        // Camera
+        [System.Serializable]
+        public class CameraConfiguration {
+            public GameObject target;
+            public Vector3 offset;
+        }
+        public CameraConfiguration cameraConfiguration;
+
+        // Displacement
+        [System.Serializable]
+        public class DisplacementConfiguration {
+            public float walkingSpeed = 1f;
+            public float runingSpeed = 1f;
+            public AnimationCurve walkRunTransition;
+            public float jumpStrength = 5f;
+        }
+        public DisplacementConfiguration displacementConfiguration;
+
+        /** Camera **/
+        public class PlayerCamera {
+            public Quaternion rotation;
+        }
+        private PlayerCamera playerCamera;
+
+        /** PHYSIC **/
+        // Displacement
+        public class Physic {
+            public Vector2 displacement;
+        }
+        private Physic physic;
+
+        /** UNITY **/
+        // Input Update
+        private void InputUpdate() {
+            // Retrieve player displacement input
+            physic.displacement = new Vector2(
+                Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical")
+                );
+        }
+
+        // Physic Update
+        private void FixedUpdate() {
+            
+        }
+
+        // Rendering Update
+        private void LateUpdate() {
+            GameObject camera = cameraConfiguration.target;
+            camera.transform.position = transform.position + cameraConfiguration.offset;
+        }
+
+
+    }
+
+}
